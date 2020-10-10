f356e00bbaa10d6a0f679f0c3d86e02d46f3854a
Poojithavb <pujitha.vb@gmail.com>
2020-10-09 17:57:26 -0700

    Added Map Functionality for Restaurant List LandingPage
    - Added Geocode for fetching lat and long for addresses
    - Added Geolocation for fetching user location

---
+++ yelp-client/src/components/Customer/Map.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import Geocode from "react-geocode";
+import CurrentLocation from './CurrentLocation'
+import { Map, GoogleApiWrapper,InfoWindow, Marker } from 'google-maps-react';
+
+const mapStyles = {
+    map: {
+      position: 'absolute',
+      width: '100%',
+      height: '100%'
+    }
+  };
+
+
+
+export class MapContainer extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            showingInfoWindow: false,  
+            activeMarker: {},          
+            selectedPlace: {}, 
+            data:[],        
+        };
+       this.getlocation();
+    }
+      
+  onMarkerClick = (props, marker, e) =>
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+  });
+
+
+onClose = props => {
+  if (this.state.showingInfoWindow) {
+    this.setState({
+      showingInfoWindow: false,
+      activeMarker: null,
+    });
+  }
+};
+
+getlocation=()=>{
+    Geocode.setApiKey('AIzaSyAkTFpnLYFTVDW-8tadbns29Kf7CpEO8Co');
+    this.props.restaurantlist.forEach((restaurent)=>{
+    let address = restaurent.address.concat(' ',restaurent.city);
+    let name = restaurent.name;
+    
+    Geocode.fromAddress(address).then(
+      response => {
+        const { lat, lng } = response.results[0].geometry.location;
+        this.state.data.push(<Marker position={{lat: lat, lng: lng}} 
+            onClick={this.onMarkerClick}
+            name={name}  
+          />)
+          this.forceUpdate()
+      },
+      error => {
+        console.error(error);
+      }
+    );
+    
+    })
+  }
+
+
+  render() {
+      console.log(this.state.data)
+    return (  
+      <CurrentLocation
+        centerAroundCurrentLocation
+        google={this.props.google}
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Current Location'}
+        />
+         {this.state.data}
+        <InfoWindow
+          marker={this.state.activeMarker}
+         
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div> 
+         </InfoWindow>  
+            {this.state.data}
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+            >
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+              </div>
+          </InfoWindow>
+        </CurrentLocation>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAkTFpnLYFTVDW-8tadbns29Kf7CpEO8Co'
+})(MapContainer);
